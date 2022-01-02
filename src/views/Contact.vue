bc79b4bc72a47158d98353f84385608534ba4a4b
minterger <minterger@gmail.com>
2022-01-02 17:57:05 -0300

    update social icons

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,22 +25,26 @@ text.value = "Contactar";
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
-          viewBox="0 0 24 24"
-          style="fill: #3b5998; msfilter: "
+          viewBox="0 0 24 18"
+          class="facebookSvg"
         >
           <path
-            d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h8.615v-6.96h-2.338v-2.725h2.338v-2c0-2.325 1.42-3.592 3.5-3.592.699-.002 1.399.034 2.095.107v2.42h-1.435c-1.128 0-1.348.538-1.348 1.325v1.735h2.697l-.35 2.725h-2.348V21H20a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1z"
+            d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"
           ></path>
         </svg>
         <!-- <img src="https://img.icons8.com/color/48/000000/facebook-new.png" /> -->
       </a>
-      <a href="https://www.instagram.com/leo_skateboard/" target="_blank">
+      <a
+        href="https://www.instagram.com/leo_skateboard/"
+        class="instagram"
+        target="_blank"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          style="fill: #f09433; msfilter: "
+          class="instagramSvg"
         >
           <path
             d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"
@@ -58,10 +62,11 @@ text.value = "Contactar";
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          style="fill: #0077b5; msfilter: "
+          class="linkedinSvg"
         >
+          <circle cx="4.983" cy="5.009" r="2.188"></circle>
           <path
-            d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"
+            d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"
           ></path>
         </svg>
         <!-- <img src="https://img.icons8.com/color/48/000000/linkedin.png" /> -->
@@ -73,10 +78,43 @@ text.value = "Contactar";
 <style scoped>
 .social-media {
   margin-top: 1rem;
+  display: flex;
+  gap: 5px;
 }
 
-.social-media a svg{
-  width: 37px;
-  height: 37px;
+.social-media a svg {
+  vertical-align: middle;
+  width: 30px;
+  height: 30px;
+}
+.instagramSvg {
+  background: radial-gradient(
+    circle at 30% 107%,
+    #fdf497 0%,
+    #fdf497 5%,
+    #fd5949 45%,
+    #d6249f 60%,
+    #285aeb 90%
+  );
+  fill: #fff;
+  border-radius: 8px;
+  width: 29px;
+  height: 29px;
+}
+
+.facebookSvg {
+  width: 29px;
+  height: 29px;
+  background: #3b5998;
+  fill: #fff;
+  border-radius: 3px;
+}
+
+.linkedinSvg {
+  width: 29px;
+  height: 29px;
+  background: #0077b5;
+  fill: #fff;
+  border-radius: 3px;
 }
 </style>
