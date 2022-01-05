e035d32d38acf106ea301a9fac6d23873303812f
minterger <minterger@gmail.com>
2022-01-04 21:59:53 -0300

    update social icons

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -20,20 +20,22 @@ text.value = "Contactar";
     <h3>O escribeme a travez de mis redes sociales</h3>
 
     <div class="social-media">
+      <!-- facebook button social -->
       <a href="https://www.facebook.com/leonelmenci" target="_blank">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
-          viewBox="0 0 24 18"
+          viewBox="0 0 22 18"
           class="facebookSvg"
         >
           <path
             d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"
           ></path>
         </svg>
-        <!-- <img src="https://img.icons8.com/color/48/000000/facebook-new.png" /> -->
       </a>
+
+      <!-- instagram button social -->
       <a
         href="https://www.instagram.com/leo_skateboard/"
         class="instagram"
@@ -54,8 +56,9 @@ text.value = "Contactar";
             d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"
           ></path>
         </svg>
-        <!-- <img src="https://img.icons8.com/color/48/000000/instagram-new.png" /> -->
       </a>
+
+      <!-- linkedin button social -->
       <a href="https://www.linkedin.com/in/leonel-menci/" target="_blank">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -69,7 +72,6 @@ text.value = "Contactar";
             d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"
           ></path>
         </svg>
-        <!-- <img src="https://img.icons8.com/color/48/000000/linkedin.png" /> -->
       </a>
     </div>
   </div>
@@ -84,8 +86,8 @@ text.value = "Contactar";
 
 .social-media a svg {
   vertical-align: middle;
-  width: 30px;
-  height: 30px;
+  width: 35px;
+  height: 35px;
 }
 .instagramSvg {
   background: radial-gradient(
@@ -98,21 +100,15 @@ text.value = "Contactar";
   );
   fill: #fff;
   border-radius: 8px;
-  width: 29px;
-  height: 29px;
 }
 
 .facebookSvg {
-  width: 29px;
-  height: 29px;
   background: #3b5998;
   fill: #fff;
   border-radius: 3px;
 }
 
 .linkedinSvg {
-  width: 29px;
-  height: 29px;
   background: #0077b5;
   fill: #fff;
   border-radius: 3px;
